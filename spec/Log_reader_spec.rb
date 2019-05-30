4f23399c0046799fcec5b93b1c896759423a329f
Oliver <ofcharrison@gmail.com>
2019-05-30 10:58:01 +0100

    rspec can read lib file - simplecov and rubocop setup - irb working

--- spec/Log_reader_spec.rb
+++ spec/Log_reader_spec.rb
@@ -1,1 +1,9 @@
-require './lib/Logreader.rb'
+# frozen_string_literal: true
+
+require 'Log_reader.rb'
+
+describe Logreader do
+  it 'can find and read the Log_reader file' do
+    expect(Logreader.new.test).to eq('FOUND')
+  end
+end
